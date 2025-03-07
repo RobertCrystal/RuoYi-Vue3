3dacd6c10cf5425132afbba7c210aa065bedba71
Robert <vx16192@outlook.com>
2025-03-07 16:53:33 +0800

    (fix)[course/expert]优化知识课程页和知识专家页

--- src/views/farm/course/index.vue
+++ src/views/farm/course/index.vue
@@ -1,38 +1,18 @@
 <template>
   <div class="app-container">
-    <el-form :model="queryParams" ref="queryRef" :inline="true" v-show="showSearch" label-width="68px">
+    <el-form :model="queryParams" ref="queryRef" :inline="true" v-show="showSearch" label-width="80px">
       <el-form-item label="课程标题" prop="title">
-        <el-input
-          v-model="queryParams.title"
-          placeholder="请输入课程标题"
-          clearable
-          @keyup.enter="handleQuery"
-        />
+        <el-input v-model="queryParams.title" placeholder="请输入课程标题" clearable @keyup.enter="handleQuery" style="width: 120px;" />
       </el-form-item>
       <el-form-item label="专家" prop="expertId">
-        <el-input
-          v-model="queryParams.expertId"
-          placeholder="请输入专家"
-          clearable
-          @keyup.enter="handleQuery"
-        />
+        <el-input v-model="queryParams.expertId" placeholder="请输入专家" clearable @keyup.enter="handleQuery" style="width: 90px;" />
       </el-form-item>
       <el-form-item label="所属领域" prop="fieldCode">
-        <el-input
-          v-model="queryParams.fieldCode"
-          placeholder="请输入所属领域"
-          clearable
-          @keyup.enter="handleQuery"
-        />
+        <el-input v-model="queryParams.fieldCode" placeholder="请输入所属领域" clearable @keyup.enter="handleQuery" style="width: 120px;" />
       </el-form-item>
       <el-form-item label="所属分类" prop="serviceCatgoryCode">
-        <el-select v-model="queryParams.serviceCatgoryCode" placeholder="请选择所属分类" clearable>
-          <el-option
-            v-for="dict in sevice_catgory"
-            :key="dict.value"
-            :label="dict.label"
-            :value="dict.value"
-          />
+        <el-select v-model="queryParams.serviceCatgoryCode" placeholder="请选择所属分类" clearable style="width: 150px;">
+          <el-option v-for="dict in sevice_catgory" :key="dict.value" :label="dict.label" :value="dict.value" />
         </el-select>
       </el-form-item>
       <el-form-item>
@@ -43,42 +23,19 @@
 
     <el-row :gutter="10" class="mb8">
       <el-col :span="1.5">
-        <el-button
-          type="primary"
-          plain
-          icon="Plus"
-          @click="handleAdd"
-          v-hasPermi="['farm:course:add']"
-        >新增</el-button>
+        <el-button type="primary" plain icon="Plus" @click="handleAdd" v-hasPermi="['farm:course:add']">新增</el-button>
       </el-col>
       <el-col :span="1.5">
-        <el-button
-          type="success"
-          plain
-          icon="Edit"
-          :disabled="single"
-          @click="handleUpdate"
-          v-hasPermi="['farm:course:edit']"
-        >修改</el-button>
+        <el-button type="success" plain icon="Edit" :disabled="single" @click="handleUpdate"
+          v-hasPermi="['farm:course:edit']">修改</el-button>
       </el-col>
       <el-col :span="1.5">
-        <el-button
-          type="danger"
-          plain
-          icon="Delete"
-          :disabled="multiple"
-          @click="handleDelete"
-          v-hasPermi="['farm:course:remove']"
-        >删除</el-button>
+        <el-button type="danger" plain icon="Delete" :disabled="multiple" @click="handleDelete"
+          v-hasPermi="['farm:course:remove']">删除</el-button>
       </el-col>
       <el-col :span="1.5">
-        <el-button
-          type="warning"
-          plain
-          icon="Download"
-          @click="handleExport"
-          v-hasPermi="['farm:course:export']"
-        >导出</el-button>
+        <el-button type="warning" plain icon="Download" @click="handleExport"
+          v-hasPermi="['farm:course:export']">导出</el-button>
       </el-col>
       <right-toolbar v-model:showSearch="showSearch" @queryTable="getList"></right-toolbar>
     </el-row>
@@ -87,35 +44,36 @@
       <el-table-column type="selection" width="55" align="center" />
       <el-table-column label="序号" type="index" align="center" prop="id" />
       <el-table-column label="课程标题" align="center" prop="title" />
-      <el-table-column label="文章摘要" align="center" prop="resume" show-overflow-tooltip class-name="description-column" />
+      <el-table-column label="文章摘要" align="center" prop="resume" show-overflow-tooltip
+        class-name="description-column" />
       <el-table-column label="专家" align="center" prop="expertId" />
       <el-table-column label="所属领域" align="center" prop="fieldCode" />
       <el-table-column label="课程封面图片" align="center" prop="image" width="100">
         <template #default="scope">
-          <image-preview :src="scope.row.image" :width="50" :height="50"/>
+          <image-preview :src="scope.row.image" :width="50" :height="50" />
         </template>
       </el-table-column>
       <el-table-column label="正文" align="center" prop="content" show-overflow-tooltip class-name="description-column" />
       <el-table-column label="所属分类" align="center" prop="serviceCatgoryCode">
         <template #default="scope">
-          <dict-tag :options="sevice_catgory" :value="scope.row.serviceCatgoryCode"/>
+          <dict-tag :options="sevice_catgory" :value="scope.row.serviceCatgoryCode" />
         </template>
       </el-table-column>
       <el-table-column label="所属品类" align="center" prop="produceCategoryId" />
       <el-table-column label="视频文件" align="center" prop="video" />
       <el-table-column label="课程类型" align="center" prop="type">
         <template #default="scope">
-          <dict-tag :options="course_type" :value="scope.row.type"/>
+          <dict-tag :options="course_type" :value="scope.row.type" />
         </template>
       </el-table-column>
       <el-table-column label="是否推荐" align="center" prop="recommend">
         <template #default="scope">
-          <dict-tag :options="recommend" :value="scope.row.recommend"/>
+          <dict-tag :options="recommend" :value="scope.row.recommend" />
         </template>
       </el-table-column>
       <el-table-column label="发布状态 " align="center" prop="publishStatus">
         <template #default="scope">
-          <dict-tag :options="publish_status" :value="scope.row.publishStatus"/>
+          <dict-tag :options="publish_status" :value="scope.row.publishStatus" />
         </template>
       </el-table-column>
       <el-table-column label="发布时间" align="center" prop="publishTime" width="180">
@@ -126,19 +84,16 @@
       <el-table-column label="浏览数量" align="center" prop="browseNum" />
       <el-table-column label="操作" align="center" class-name="small-padding fixed-width">
         <template #default="scope">
-          <el-button link type="primary" icon="Edit" @click="handleUpdate(scope.row)" v-hasPermi="['farm:course:edit']">修改</el-button>
-          <el-button link type="primary" icon="Delete" @click="handleDelete(scope.row)" v-hasPermi="['farm:course:remove']">删除</el-button>
+          <el-button link type="primary" icon="Edit" @click="handleUpdate(scope.row)"
+            v-hasPermi="['farm:course:edit']">修改</el-button>
+          <el-button link type="primary" icon="Delete" @click="handleDelete(scope.row)"
+            v-hasPermi="['farm:course:remove']">删除</el-button>
         </template>
       </el-table-column>
     </el-table>
-    
-    <pagination
-      v-show="total>0"
-      :total="total"
-      v-model:page="queryParams.pageNum"
-      v-model:limit="queryParams.pageSize"
-      @pagination="getList"
-    />
+
+    <pagination v-show="total > 0" :total="total" v-model:page="queryParams.pageNum" v-model:limit="queryParams.pageSize"
+      @pagination="getList" />
 
     <!-- 添加或修改知识课程对话框 -->
     <el-dialog :title="title" v-model="open" width="500px" append-to-body>
@@ -156,19 +111,15 @@
           <el-input v-model="form.fieldCode" placeholder="请输入所属领域" />
         </el-form-item>
         <el-form-item label="课程封面图片" prop="image">
-          <image-upload v-model="form.image"/>
+          <image-upload v-model="form.image" />
         </el-form-item>
         <el-form-item label="正文">
-          <editor v-model="form.content" :min-height="192"/>
+          <editor v-model="form.content" :min-height="192" />
         </el-form-item>
         <el-form-item label="所属分类" prop="serviceCatgoryCode">
           <el-select v-model="form.serviceCatgoryCode" placeholder="请选择所属分类">
-            <el-option
-              v-for="dict in sevice_catgory"
-              :key="dict.value"
-              :label="dict.label"
-              :value="dict.value"
-            ></el-option>
+            <el-option v-for="dict in sevice_catgory" :key="dict.value" :label="dict.label"
+              :value="dict.value"></el-option>
           </el-select>
         </el-form-item>
         <el-form-item label="所属品类" prop="produceCategoryId">
@@ -179,39 +130,23 @@
         </el-form-item>
         <el-form-item label="课程类型" prop="type">
           <el-select v-model="form.type" placeholder="请选择课程类型">
-            <el-option
-              v-for="dict in course_type"
-              :key="dict.value"
-              :label="dict.label"
-              :value="dict.value"
-            ></el-option>
+            <el-option v-for="dict in course_type" :key="dict.value" :label="dict.label"
+              :value="dict.value"></el-option>
           </el-select>
         </el-form-item>
         <el-form-item label="是否推荐" prop="recommend">
           <el-select v-model="form.recommend" placeholder="请选择是否推荐">
-            <el-option
-              v-for="dict in recommend"
-              :key="dict.value"
-              :label="dict.label"
-              :value="dict.value"
-            ></el-option>
+            <el-option v-for="dict in recommend" :key="dict.value" :label="dict.label" :value="dict.value"></el-option>
           </el-select>
         </el-form-item>
         <el-form-item label="发布状态 " prop="publishStatus">
           <el-select v-model="form.publishStatus" placeholder="请选择发布状态 ">
-            <el-option
-              v-for="dict in publish_status"
-              :key="dict.value"
-              :label="dict.label"
-              :value="dict.value"
-            ></el-option>
+            <el-option v-for="dict in publish_status" :key="dict.value" :label="dict.label"
+              :value="dict.value"></el-option>
           </el-select>
         </el-form-item>
         <el-form-item label="发布时间" prop="publishTime">
-          <el-date-picker clearable
-            v-model="form.publishTime"
-            type="date"
-            value-format="YYYY-MM-DD"
+          <el-date-picker clearable v-model="form.publishTime" type="date" value-format="YYYY-MM-DD"
             placeholder="请选择发布时间">
           </el-date-picker>
         </el-form-item>
@@ -379,12 +314,12 @@ function submitForm() {
 /** 删除按钮操作 */
 function handleDelete(row) {
   const _ids = row.id || ids.value;
-  proxy.$modal.confirm('是否确认删除知识课程编号为"' + _ids + '"的数据项？').then(function() {
+  proxy.$modal.confirm('是否确认删除知识课程编号为"' + _ids + '"的数据项？').then(function () {
     return delCourse(_ids);
   }).then(() => {
     getList();
     proxy.$modal.msgSuccess("删除成功");
-  }).catch(() => {});
+  }).catch(() => { });
 }
 
 /** 导出按钮操作 */
--- src/views/farm/expert/index.vue
+++ src/views/farm/expert/index.vue
@@ -2,84 +2,17 @@
   <div class="app-container">
     <el-form :model="queryParams" ref="queryRef" :inline="true" v-show="showSearch" label-width="68px">
       <el-form-item label="专家名称" prop="name">
-        <el-input
-          v-model="queryParams.name"
-          placeholder="请输入专家名称"
-          clearable
-          @keyup.enter="handleQuery"
-        />
+        <el-input v-model="queryParams.name" placeholder="请输入专家名称" clearable @keyup.enter="handleQuery" />
       </el-form-item>
       <el-form-item label="所属领域" prop="fieldCode">
-        <el-input
-          v-model="queryParams.fieldCode"
-          placeholder="请输入所属领域"
-          clearable
-          @keyup.enter="handleQuery"
-        />
-      </el-form-item>
-      <el-form-item label="专家简介" prop="description">
-        <el-input
-          v-model="queryParams.description"
-          placeholder="请输入专家简介"
-          clearable
-          @keyup.enter="handleQuery"
-        />
-      </el-form-item>
-      <el-form-item label="所属机构" prop="organization">
-        <el-input
-          v-model="queryParams.organization"
-          placeholder="请输入所属机构"
-          clearable
-          @keyup.enter="handleQuery"
-        />
+        <el-select v-model="queryParams.fieldCode" placeholder="请选择所属领域" clearable style="width: 160px;">
+          <el-option v-for="dict in expert_field" :key="dict.value" :label="dict.label" :value="dict.value" />
+        </el-select>
       </el-form-item>
       <el-form-item label="专家等级" prop="levelCode">
-        <el-input
-          v-model="queryParams.levelCode"
-          placeholder="请输入专家等级"
-          clearable
-          @keyup.enter="handleQuery"
-        />
-      </el-form-item>
-      <el-form-item label="发布时间" prop="publishTime">
-        <el-date-picker clearable
-          v-model="queryParams.publishTime"
-          type="date"
-          value-format="YYYY-MM-DD"
-          placeholder="请选择发布时间">
-        </el-date-picker>
-      </el-form-item>
-      <el-form-item label="是否推荐：1.推荐；2.不推荐" prop="recommend">
-        <el-input
-          v-model="queryParams.recommend"
-          placeholder="请输入是否推荐：1.推荐；2.不推荐"
-          clearable
-          @keyup.enter="handleQuery"
-        />
-      </el-form-item>
-      <el-form-item label="浏览数量" prop="browseNum">
-        <el-input
-          v-model="queryParams.browseNum"
-          placeholder="请输入浏览数量"
-          clearable
-          @keyup.enter="handleQuery"
-        />
-      </el-form-item>
-      <el-form-item label="创建时间" prop="createdTime">
-        <el-date-picker clearable
-          v-model="queryParams.createdTime"
-          type="date"
-          value-format="YYYY-MM-DD"
-          placeholder="请选择创建时间">
-        </el-date-picker>
-      </el-form-item>
-      <el-form-item label="更新时间" prop="updatedTime">
-        <el-date-picker clearable
-          v-model="queryParams.updatedTime"
-          type="date"
-          value-format="YYYY-MM-DD"
-          placeholder="请选择更新时间">
-        </el-date-picker>
+        <el-select v-model="queryParams.levelCode" placeholder="请选择专家等级" clearable style="width: 150px;">
+          <el-option v-for="dict in expert_level" :key="dict.value" :label="dict.label" :value="dict.value" />
+        </el-select>
       </el-form-item>
       <el-form-item>
         <el-button type="primary" icon="Search" @click="handleQuery">搜索</el-button>
@@ -89,92 +22,73 @@
 
     <el-row :gutter="10" class="mb8">
       <el-col :span="1.5">
-        <el-button
-          type="primary"
-          plain
-          icon="Plus"
-          @click="handleAdd"
-          v-hasPermi="['farm:expert:add']"
-        >新增</el-button>
+        <el-button type="primary" plain icon="Plus" @click="handleAdd" v-hasPermi="['farm:expert:add']">新增</el-button>
       </el-col>
       <el-col :span="1.5">
-        <el-button
-          type="success"
-          plain
-          icon="Edit"
-          :disabled="single"
-          @click="handleUpdate"
-          v-hasPermi="['farm:expert:edit']"
-        >修改</el-button>
+        <el-button type="success" plain icon="Edit" :disabled="single" @click="handleUpdate"
+          v-hasPermi="['farm:expert:edit']">修改</el-button>
       </el-col>
       <el-col :span="1.5">
-        <el-button
-          type="danger"
-          plain
-          icon="Delete"
-          :disabled="multiple"
-          @click="handleDelete"
-          v-hasPermi="['farm:expert:remove']"
-        >删除</el-button>
+        <el-button type="danger" plain icon="Delete" :disabled="multiple" @click="handleDelete"
+          v-hasPermi="['farm:expert:remove']">删除</el-button>
       </el-col>
       <el-col :span="1.5">
-        <el-button
-          type="warning"
-          plain
-          icon="Download"
-          @click="handleExport"
-          v-hasPermi="['farm:expert:export']"
-        >导出</el-button>
+        <el-button type="warning" plain icon="Download" @click="handleExport"
+          v-hasPermi="['farm:expert:export']">导出</el-button>
       </el-col>
       <right-toolbar v-model:showSearch="showSearch" @queryTable="getList"></right-toolbar>
     </el-row>
 
     <el-table v-loading="loading" :data="expertList" @selection-change="handleSelectionChange">
       <el-table-column type="selection" width="55" align="center" />
-      <el-table-column label="主键" align="center" prop="id" />
+      <el-table-column label="序号" type="index" align="center" prop="id" />
       <el-table-column label="专家名称" align="center" prop="name" />
-      <el-table-column label="所属领域" align="center" prop="fieldCode" />
-      <el-table-column label="专家简介" align="center" prop="description" />
-      <el-table-column label="头像图片id" align="center" prop="image" width="100">
+      <el-table-column label="所属领域" align="center" prop="fieldCode">
         <template #default="scope">
-          <image-preview :src="scope.row.image" :width="50" :height="50"/>
+          <dict-tag :options="expert_field" :value="scope.row.fieldCode" />
+        </template>
+      </el-table-column>
+      <el-table-column label="专家简介" align="center" prop="description" show-overflow-tooltip
+        class-name="description-column" />
+      <el-table-column label="头像" align="center" prop="image" width="100">
+        <template #default="scope">
+          <image-preview :src="scope.row.image" :width="50" :height="50" />
         </template>
       </el-table-column>
       <el-table-column label="所属机构" align="center" prop="organization" />
-      <el-table-column label="专家等级" align="center" prop="levelCode" />
+      <el-table-column label="专家等级" align="center" prop="levelCode">
+        <template #default="scope">
+          <dict-tag :options="expert_level" :value="scope.row.levelCode" />
+        </template>
+      </el-table-column>
       <el-table-column label="发布时间" align="center" prop="publishTime" width="180">
         <template #default="scope">
           <span>{{ parseTime(scope.row.publishTime, '{y}-{m}-{d}') }}</span>
         </template>
       </el-table-column>
-      <el-table-column label="发布状态：1.已发布；2.未发布" align="center" prop="publishStatus" />
-      <el-table-column label="是否推荐：1.推荐；2.不推荐" align="center" prop="recommend" />
+      <el-table-column label="发布状态" align="center" prop="publishStatus">
+        <template #default="scope">
+          <dict-tag :options="publish_status" :value="scope.row.publishStatus" />
+        </template>
+      </el-table-column>
+      <el-table-column label="是否推荐" align="center" prop="recommend">
+        <template #default="scope">
+          <dict-tag :options="recommend" :value="scope.row.recommend" />
+        </template>
+      </el-table-column>
       <el-table-column label="浏览数量" align="center" prop="browseNum" />
-      <el-table-column label="创建时间" align="center" prop="createdTime" width="180">
-        <template #default="scope">
-          <span>{{ parseTime(scope.row.createdTime, '{y}-{m}-{d}') }}</span>
-        </template>
-      </el-table-column>
-      <el-table-column label="更新时间" align="center" prop="updatedTime" width="180">
-        <template #default="scope">
-          <span>{{ parseTime(scope.row.updatedTime, '{y}-{m}-{d}') }}</span>
-        </template>
-      </el-table-column>
       <el-table-column label="操作" align="center" class-name="small-padding fixed-width">
         <template #default="scope">
-          <el-button link type="primary" icon="Edit" @click="handleUpdate(scope.row)" v-hasPermi="['farm:expert:edit']">修改</el-button>
-          <el-button link type="primary" icon="Delete" @click="handleDelete(scope.row)" v-hasPermi="['farm:expert:remove']">删除</el-button>
+          <el-button link type="primary" icon="Edit" @click="handleUpdate(scope.row)"
+            v-hasPermi="['farm:expert:edit']">修改</el-button>
+          <el-button link type="primary" icon="Delete" @click="handleDelete(scope.row)"
+            v-hasPermi="['farm:expert:remove']">删除</el-button>
         </template>
       </el-table-column>
     </el-table>
-    
-    <pagination
-      v-show="total>0"
-      :total="total"
-      v-model:page="queryParams.pageNum"
-      v-model:limit="queryParams.pageSize"
-      @pagination="getList"
-    />
+
+    <pagination v-show="total > 0" :total="total" v-model:page="queryParams.pageNum" v-model:limit="queryParams.pageSize"
+      @pagination="getList" />
 
     <!-- 添加或修改知识专家对话框 -->
     <el-dialog :title="title" v-model="open" width="500px" append-to-body>
@@ -183,49 +97,44 @@
           <el-input v-model="form.name" placeholder="请输入专家名称" />
         </el-form-item>
         <el-form-item label="所属领域" prop="fieldCode">
-          <el-input v-model="form.fieldCode" placeholder="请输入所属领域" />
+          <el-select v-model="form.fieldCode" placeholder="请选择所属领域">
+            <el-option v-for="dict in expert_field" :key="dict.value" :label="dict.label"
+              :value="dict.value"></el-option>
+          </el-select>
         </el-form-item>
         <el-form-item label="专家简介" prop="description">
           <el-input v-model="form.description" placeholder="请输入专家简介" />
         </el-form-item>
-        <el-form-item label="头像图片id" prop="image">
-          <image-upload v-model="form.image"/>
+        <el-form-item label="头像" prop="image">
+          <image-upload v-model="form.image" />
         </el-form-item>
         <el-form-item label="所属机构" prop="organization">
           <el-input v-model="form.organization" placeholder="请输入所属机构" />
         </el-form-item>
         <el-form-item label="专家等级" prop="levelCode">
-          <el-input v-model="form.levelCode" placeholder="请输入专家等级" />
+          <el-select v-model="form.levelCode" placeholder="请选择专家等级">
+            <el-option v-for="dict in expert_level" :key="dict.value" :label="dict.label"
+              :value="dict.value"></el-option>
+          </el-select>
         </el-form-item>
         <el-form-item label="发布时间" prop="publishTime">
-          <el-date-picker clearable
-            v-model="form.publishTime"
-            type="date"
-            value-format="YYYY-MM-DD"
+          <el-date-picker clearable v-model="form.publishTime" type="date" value-format="YYYY-MM-DD"
             placeholder="请选择发布时间">
           </el-date-picker>
         </el-form-item>
-        <el-form-item label="是否推荐：1.推荐；2.不推荐" prop="recommend">
-          <el-input v-model="form.recommend" placeholder="请输入是否推荐：1.推荐；2.不推荐" />
+        <el-form-item label="发布状态" prop="publishStatus">
+          <el-select v-model="form.publishStatus" placeholder="请选择发布状态">
+            <el-option v-for="dict in publish_status" :key="dict.value" :label="dict.label"
+              :value="dict.value"></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="是否推荐" prop="recommend">
+          <el-select v-model="form.recommend" placeholder="请选择是否推荐">
+            <el-option v-for="dict in recommend" :key="dict.value" :label="dict.label" :value="dict.value"></el-option>
+          </el-select>
         </el-form-item>
         <el-form-item label="浏览数量" prop="browseNum">
           <el-input v-model="form.browseNum" placeholder="请输入浏览数量" />
-        </el-form-item>
-        <el-form-item label="创建时间" prop="createdTime">
-          <el-date-picker clearable
-            v-model="form.createdTime"
-            type="date"
-            value-format="YYYY-MM-DD"
-            placeholder="请选择创建时间">
-          </el-date-picker>
-        </el-form-item>
-        <el-form-item label="更新时间" prop="updatedTime">
-          <el-date-picker clearable
-            v-model="form.updatedTime"
-            type="date"
-            value-format="YYYY-MM-DD"
-            placeholder="请选择更新时间">
-          </el-date-picker>
         </el-form-item>
       </el-form>
       <template #footer>
@@ -238,10 +147,22 @@
   </div>
 </template>
 
+<style scoped>
+.description-column .cell {
+  /* 确保单行显示 */
+  white-space: nowrap;
+  /* 超出部分隐藏 */
+  overflow: hidden;
+  /* 超出部分用...表示 */
+  text-overflow: ellipsis;
+}
+</style>
+
 <script setup name="Expert">
 import { listExpert, getExpert, delExpert, addExpert, updateExpert } from "@/api/farm/expert";
 
 const { proxy } = getCurrentInstance();
+const { recommend, expert_field, publish_status, expert_level } = proxy.useDict('recommend', 'expert_field', 'publish_status', 'expert_level');
 
 const expertList = ref([]);
 const open = ref(false);
@@ -260,20 +181,23 @@ const data = reactive({
     pageSize: 10,
     name: null,
     fieldCode: null,
-    description: null,
-    image: null,
-    organization: null,
     levelCode: null,
-    publishTime: null,
-    publishStatus: null,
-    recommend: null,
-    browseNum: null,
-    createdTime: null,
-    updatedTime: null
   },
   rules: {
     name: [
       { required: true, message: "专家名称不能为空", trigger: "blur" }
+    ],
+    fieldCode: [
+      { required: true, message: "所属领域不能为空", trigger: "change" }
+    ],
+    levelCode: [
+      { required: true, message: "专家等级不能为空", trigger: "change" }
+    ],
+    publishStatus: [
+      { required: true, message: "发布状态不能为空", trigger: "change" }
+    ],
+    recommend: [
+      { required: true, message: "是否推荐不能为空", trigger: "change" }
     ],
   }
 });
@@ -377,12 +301,12 @@ function submitForm() {
 /** 删除按钮操作 */
 function handleDelete(row) {
   const _ids = row.id || ids.value;
-  proxy.$modal.confirm('是否确认删除知识专家编号为"' + _ids + '"的数据项？').then(function() {
+  proxy.$modal.confirm('是否确认删除知识专家编号为"' + _ids + '"的数据项？').then(function () {
     return delExpert(_ids);
   }).then(() => {
     getList();
     proxy.$modal.msgSuccess("删除成功");
-  }).catch(() => {});
+  }).catch(() => { });
 }
 
 /** 导出按钮操作 */
