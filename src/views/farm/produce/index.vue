afabf86b4f159a98cc157e1c638ecb724fcabaef
Robert <vx16192@outlook.com>
2025-03-07 09:30:07 +0800

    (fix)[produce]修改了农产品页面的一些布局

--- src/views/farm/produce/index.vue
+++ src/views/farm/produce/index.vue
@@ -2,39 +2,19 @@
   <div class="app-container">
     <el-form :model="queryParams" ref="queryRef" :inline="true" v-show="showSearch" label-width="100px">
       <el-form-item label="农产品名称" prop="title">
-        <el-input
-          v-model="queryParams.title"
-          placeholder="请输入农产品名称"
-          clearable
-          @keyup.enter="handleQuery"
-        />
+        <el-input v-model="queryParams.title" placeholder="请输入农产品名称" clearable @keyup.enter="handleQuery" style="width: 150px;" />
       </el-form-item>
       <el-form-item label="卖点摘要" prop="resume">
-        <el-input
-          v-model="queryParams.resume"
-          placeholder="请输入卖点摘要"
-          clearable
-          @keyup.enter="handleQuery"
-        />
+        <el-input v-model="queryParams.resume" placeholder="请输入卖点摘要" clearable @keyup.enter="handleQuery" style="width: 150px;" />
       </el-form-item>
       <el-form-item label="上架状态" prop="pushStatus">
-        <el-select v-model="queryParams.pushStatus" placeholder="请选择上架状态" clearable>
-          <el-option
-            v-for="dict in push_status"
-            :key="dict.value"
-            :label="dict.label"
-            :value="dict.value"
-          />
+        <el-select v-model="queryParams.pushStatus" placeholder="请选择上架状态" clearable style="width: 150px;">
+          <el-option v-for="dict in push_status" :key="dict.value" :label="dict.label" :value="dict.value" />
         </el-select>
       </el-form-item>
       <el-form-item label="是否推荐" prop="recommend">
-        <el-select v-model="queryParams.recommend" placeholder="请选择是否推荐" clearable>
-          <el-option
-            v-for="dict in recommend"
-            :key="dict.value"
-            :label="dict.label"
-            :value="dict.value"
-          />
+        <el-select v-model="queryParams.recommend" placeholder="请选择是否推荐" clearable style="width: 150px;">
+          <el-option v-for="dict in recommend" :key="dict.value" :label="dict.label" :value="dict.value" />
         </el-select>
       </el-form-item>
       <el-form-item>
@@ -45,42 +25,19 @@
 
     <el-row :gutter="10" class="mb8">
       <el-col :span="1.5">
-        <el-button
-          type="primary"
-          plain
-          icon="Plus"
-          @click="handleAdd"
-          v-hasPermi="['farm:produce:add']"
-        >新增</el-button>
+        <el-button type="primary" plain icon="Plus" @click="handleAdd" v-hasPermi="['farm:produce:add']">新增</el-button>
       </el-col>
       <el-col :span="1.5">
-        <el-button
-          type="success"
-          plain
-          icon="Edit"
-          :disabled="single"
-          @click="handleUpdate"
-          v-hasPermi="['farm:produce:edit']"
-        >修改</el-button>
+        <el-button type="success" plain icon="Edit" :disabled="single" @click="handleUpdate"
+          v-hasPermi="['farm:produce:edit']">修改</el-button>
       </el-col>
       <el-col :span="1.5">
-        <el-button
-          type="danger"
-          plain
-          icon="Delete"
-          :disabled="multiple"
-          @click="handleDelete"
-          v-hasPermi="['farm:produce:remove']"
-        >删除</el-button>
+        <el-button type="danger" plain icon="Delete" :disabled="multiple" @click="handleDelete"
+          v-hasPermi="['farm:produce:remove']">删除</el-button>
       </el-col>
       <el-col :span="1.5">
-        <el-button
-          type="warning"
-          plain
-          icon="Download"
-          @click="handleExport"
-          v-hasPermi="['farm:produce:export']"
-        >导出</el-button>
+        <el-button type="warning" plain icon="Download" @click="handleExport"
+          v-hasPermi="['farm:produce:export']">导出</el-button>
       </el-col>
       <right-toolbar v-model:showSearch="showSearch" @queryTable="getList"></right-toolbar>
     </el-row>
@@ -91,7 +48,7 @@
       <el-table-column label="农产品名称" align="center" prop="title" />
       <el-table-column label="图片" align="center" prop="image" width="100">
         <template #default="scope">
-          <image-preview :src="scope.row.image" :width="50" :height="50"/>
+          <image-preview :src="scope.row.image" :width="50" :height="50" />
         </template>
       </el-table-column>
       <el-table-column label="卖点摘要" align="center" prop="resume" />
@@ -104,31 +61,28 @@
       </el-table-column>
       <el-table-column label="上架状态" align="center" prop="pushStatus">
         <template #default="scope">
-          <dict-tag :options="push_status" :value="scope.row.pushStatus"/>
+          <dict-tag :options="push_status" :value="scope.row.pushStatus" />
         </template>
       </el-table-column>
       <el-table-column label="是否推荐" align="center" prop="recommend">
         <template #default="scope">
-          <dict-tag :options="recommend" :value="scope.row.recommend"/>
+          <dict-tag :options="recommend" :value="scope.row.recommend" />
         </template>
       </el-table-column>
       <el-table-column label="创建人" align="center" prop="accountId" />
       <el-table-column label="浏览量" align="center" prop="browseNum" />
       <el-table-column label="操作" align="center" class-name="small-padding fixed-width">
         <template #default="scope">
-          <el-button link type="primary" icon="Edit" @click="handleUpdate(scope.row)" v-hasPermi="['farm:produce:edit']">修改</el-button>
-          <el-button link type="primary" icon="Delete" @click="handleDelete(scope.row)" v-hasPermi="['farm:produce:remove']">删除</el-button>
+          <el-button link type="primary" icon="Edit" @click="handleUpdate(scope.row)"
+            v-hasPermi="['farm:produce:edit']">修改</el-button>
+          <el-button link type="primary" icon="Delete" @click="handleDelete(scope.row)"
+            v-hasPermi="['farm:produce:remove']">删除</el-button>
         </template>
       </el-table-column>
     </el-table>
-    
-    <pagination
-      v-show="total>0"
-      :total="total"
-      v-model:page="queryParams.pageNum"
-      v-model:limit="queryParams.pageSize"
-      @pagination="getList"
-    />
+
+    <pagination v-show="total > 0" :total="total" v-model:page="queryParams.pageNum" v-model:limit="queryParams.pageSize"
+      @pagination="getList" />
 
     <!-- 添加或修改农产品对话框 -->
     <el-dialog :title="title" v-model="open" width="500px" append-to-body>
@@ -137,7 +91,7 @@
           <el-input v-model="form.title" placeholder="请输入农产品名称" />
         </el-form-item>
         <el-form-item label="图片" prop="image">
-          <image-upload v-model="form.image"/>
+          <image-upload v-model="form.image" />
         </el-form-item>
         <el-form-item label="卖点摘要" prop="resume">
           <el-input v-model="form.resume" placeholder="请输入卖点摘要" />
@@ -153,22 +107,13 @@
         </el-form-item>
         <el-form-item label="上架状态" prop="pushStatus">
           <el-select v-model="form.pushStatus" placeholder="请选择上架状态">
-            <el-option
-              v-for="dict in push_status"
-              :key="dict.value"
-              :label="dict.label"
-              :value="dict.value"
-            ></el-option>
+            <el-option v-for="dict in push_status" :key="dict.value" :label="dict.label"
+              :value="dict.value"></el-option>
           </el-select>
         </el-form-item>
         <el-form-item label="是否推荐" prop="recommend">
           <el-select v-model="form.recommend" placeholder="请选择是否推荐">
-            <el-option
-              v-for="dict in recommend"
-              :key="dict.value"
-              :label="dict.label"
-              :value="dict.value"
-            ></el-option>
+            <el-option v-for="dict in recommend" :key="dict.value" :label="dict.label" :value="dict.value"></el-option>
           </el-select>
         </el-form-item>
         <el-form-item label="产品详情" prop="description">
@@ -326,12 +271,12 @@ function submitForm() {
 /** 删除按钮操作 */
 function handleDelete(row) {
   const _ids = row.id || ids.value;
-  proxy.$modal.confirm('是否确认删除农产品编号为"' + _ids + '"的数据项？').then(function() {
+  proxy.$modal.confirm('是否确认删除农产品编号为"' + _ids + '"的数据项？').then(function () {
     return delProduce(_ids);
   }).then(() => {
     getList();
     proxy.$modal.msgSuccess("删除成功");
-  }).catch(() => {});
+  }).catch(() => { });
 }
 
 /** 导出按钮操作 */
